473a975cad09e5334adc9ff0044dd04716e73c46
raylu15742cs <raylucs15742@gmail.com>
2022-09-02 13:12:28 -0700

    finish random number generator

---
+++ index.js
@@ -0,0 +1,12 @@
+let generate = () => {
+    let current = Math.floor(Math.random() * 100)
+
+    let target = document.getElementById("main")
+    if(target > 90) {
+        target.innerHTML += `You are Rewarded ${current}`
+    }else {
+        target.innerHTML += `Try Again Tomorrow ${current}`
+    }
+}
+
+generate();
