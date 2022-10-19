8c7fe42fc1dd183158cbb7cf254643def3c1f3eb
raylu15742cs <raylucs15742@gmail.com>
2022-10-18 17:27:47 -0700

    fix case 90

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let generate = () => {
     let current = Math.floor(Math.random() * 10 + 90)
 
     let target = document.getElementById("main")
-    if(current > 90) {
+    if(current >= 90) {
         target.innerHTML += `<h1>You are Rewarded ${current} , bubba :) </h1>`
     }else {
         target.innerHTML += `<h1>Try Again Tomorrow ${current}</h1>`
